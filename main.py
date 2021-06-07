d1e70c3c74f7788d3d35cf9c95da85418134d4a6
Jeffery Erhunse <>
2021-06-07 20:15:10 +0000

    -create a selector function -create function to find followers in selected list item

--- main.py
+++ main.py
@@ -4,19 +4,42 @@ from game_data import data
 import random
 
 print(logo)
-#Randomly select 2 celebrities from list 
-choice1 = random.choice(data)
-choice2 = random.choice(data)
+#Randomly generate a cleb
+def selector():
+    choice = random.choice(data)
+    return choice 
 
-#assign their followers to a variable 
-choice1_followers = (choice1["follower_count"],choice1["name"])
-choice2_followers = (choice2["follower_count"],choice2["name"])
+print(selector())
 
-def compare_items(first_info, first_follower, sec_info, sec_follower):
-  if first fo
-  print(f"Compare{input1}")
-  print(vs)
-  print(f"Against{input2}")
-  
-compare_items(choice1,choice2)
+def find_follower_count(celebrity):
+  followers = celebrity["follower_count"]
+  return followers
+
+
+# #assign their followers to a variable 
+# choice1_followers = (choice1["follower_count"],choice1["name"])
+# choice2_followers = (choice2["follower_count"],choice2["name"])
+
+# def compare_items(first_info, first_follower, sec_info, sec_follower):
+#   winner = []
+#   points = 0
+#   next_starter = []
+#   if first_follower > sec_follower:
+#     winner = "a"
+#     next_starter = first_info
+#   elif first_follower < sec_follower:
+#     winner = "b"
+#     next_starter = sec_info
+
+#   print(f"Compare{first_info}")
+#   print(vs)
+#   print(f"Against{sec_info}")
+
+#   guess = input("Select A or B: ")
+#   if guess == winner:
+#     points += 1
+#     return (f"You got it! Current score {points} \n")
+#   else:
+#     print(f"Wrong answer, your final score was {points}")
+#   return next_starter
 
