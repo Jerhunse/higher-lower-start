ff8456ed06c59ec8af6cc76a230d666708110bd9
Jeffery Erhunse <>
2021-06-07 17:24:27 +0000

    created compare function

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ choice2 = random.choice(data)
 choice1_followers = (choice1["follower_count"],choice1["name"])
 choice2_followers = (choice2["follower_count"],choice2["name"])
 
-def compare_items(first_info, first):
-  if input1
+def compare_items(first_info, first_follower, sec_info, sec_follower):
+  if first fo
   print(f"Compare{input1}")
   print(vs)
   print(f"Against{input2}")
