313853cdc447a6c4613b3eb7e48e9b57937b07a8
Jeffery Erhunse <>
2021-06-07 17:23:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from art import logo
+from art import vs
+from game_data import data
+import random
+
+print(logo)
+#Randomly select 2 celebrities from list 
+choice1 = random.choice(data)
+choice2 = random.choice(data)
+
+#assign their followers to a variable 
+choice1_followers = (choice1["follower_count"],choice1["name"])
+choice2_followers = (choice2["follower_count"],choice2["name"])
+
+def compare_items(first_info, first):
+  if input1
+  print(f"Compare{input1}")
+  print(vs)
+  print(f"Against{input2}")
+  
+compare_items(choice1,choice2)
+
